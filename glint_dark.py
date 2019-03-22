fd4ada947de5d940726e52b23c7a887e65ecc503
mam <4233805+mamartinod@users.noreply.github.com>
2019-03-22 16:29:43 +1100

    Update glint_dark.py
    Add creation and saving of dark pdf.
    Add monitoring plots

--- glint_dark.py
+++ glint_dark.py
@@ -9,41 +9,125 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import glint_classes
+import h5py
 
+def save(path, dic_data, date):
+    '''
+    Save the histogram of the dark
+    ------------------------------
+    path : string, path of the file to save. Must contain the name of the file
+    dic_data : dictionary of data to save
+    date : string, date of the acquisition of the data (YYYY-MM-DD)
+    '''
+    # Check if saved file exist
+    if os.path.exists(path):
+        opening_mode = 'w' # Overwright the whole existing file.
+    else:
+        opening_mode = 'a' # Create a new file at "path"
+        
+    with h5py.File(path, opening_mode) as f:
+        f.attrs['date'] = date
+        
+        f.create_group('dark')
+        for key in dic_data.keys():
+            f.create_dataset('dark/%s'%(key), data=dic_data[key])
+            
 ''' Inputs '''
 datafolder = '201806_alfBoo/'
-root = "C:/glint/"
-data_path = root+'data/'+datafolder
+root = "C:/glint_data/"
+data_path = root+datafolder
 dark_list = [data_path+f for f in os.listdir(data_path) if 'dark' in f]
+date = '2018-06-25'
 
 ''' Output '''
-output_path = root+'reduction/'+datafolder
+output_path = 'C:/glint/reduction/'+datafolder
 if not os.path.exists(output_path):
     os.makedirs(output_path)
+    
+''' Monitoring '''
+''' Check the non-uniformities and defect pixels of the darks '''
+monitor = False
+if monitor:
+    avg_dark = [] # Average dark current (in count) per frame
+    var_dark = [] # Variance of dark current (in count) per frame
+    diff_dark = [] # Different between a pixel value and the average dark current
     
 ''' Computing average dark '''
 superDark = np.zeros((344,96))
 superNbImg = 0.
 
-misc = []
+
+list_hist = []
+bin_hist, step = np.linspace(-3000, 2**14, 1000, retstep=True)
+bin_hist_cent = bin_hist[:-1] + step/2
+
 
 for f in dark_list[:]:
-#    print("------------\nProcess of : ", f)
+    print("Process of : %s (%d / %d)" %(f, dark_list.index(f)+1, len(dark_list)))
     dark = glint_classes.File(f)
     
     superDark = superDark + dark.data.sum(axis=0)
     superNbImg = superNbImg + dark.nbimg
-    misc.append(dark.data.mean(axis=(1,2)))
-        
+
+    hist = np.histogram(np.ravel(dark.data - dark.data.mean(axis=(1,2))[:,None,None]), bins=bin_hist)
+    list_hist.append(hist[0])
+    
+    if monitor:
+        avg_dark.append(dark.data.mean(axis=(1,2)))
+        var_dark.append(dark.data.var(axis=(1,2)))
+        diff_dark.append(dark.data - dark.data.mean(axis=(1,2))[:,None,None])
 
 if superNbImg != 0.:
     superDark /= superNbImg
     np.save(output_path+'superDark', superDark)
 
-misc = [selt for elt in misc for selt in elt]    
-misc = np.array(misc)
+list_hist = np.array(list_hist)
+super_hist = np.sum(list_hist, axis=0)
+
+save(output_path+'hist_dark.hdf5', {'histogram':super_hist, 'cent_bins':bin_hist_cent}, date)
+
 plt.figure()
-plt.plot(misc)
+plt.semilogy(bin_hist_cent, super_hist)
 plt.grid()
-plt.xlabel('frame')
-plt.ylabel('Average dark current (ADU)')
+plt.xlabel('Dark current (ADU)', size=38)
+plt.ylabel('Count', size=38)
+plt.xticks(size=36);plt.yticks(size=36)
+plt.title('Histogram of the dark current', size=40)
+
+''' Inspecting non-uniformities '''
+if monitor:
+    avg_dark = np.array([selt for elt in avg_dark for selt in elt])
+    var_dark = np.array([selt for elt in var_dark for selt in elt])
+    diff_dark = np.array([selt for elt in diff_dark for selt in elt])
+    
+    hist_avg, bin_avg = np.histogram(avg_dark, bins=int(len(avg_dark)**0.5))
+    hist_var, bin_var = np.histogram(var_dark, bins=int(len(var_dark)**0.5))
+    hist_diff, bin_diff = np.histogram(diff_dark, bins=int(len(diff_dark)**0.5))
+    
+    bin_avg_cent = bin_avg[:-1] + np.diff(bin_avg)/2
+    bin_var_cent = bin_var[:-1] + np.diff(bin_var)/2
+    bin_diff_cent = bin_diff[:-1] + np.diff(bin_diff)/2
+    
+    plt.figure()
+    plt.title('Histogram of average dark', size=40)
+    plt.semilogy(bin_avg_cent, hist_avg, lw=3)
+    plt.grid()
+    plt.xlabel('Average dark', size=40)
+    plt.ylabel('Count', size=40)
+    plt.xticks(size=40);plt.yticks(size=40)
+    
+    plt.figure()
+    plt.title('Histogram of variance of dark', size=40)
+    plt.semilogy(bin_var_cent, hist_var, lw=3)
+    plt.grid()
+    plt.xlabel('Average dark', size=40)
+    plt.ylabel('Count', size=40)
+    plt.xticks(size=40);plt.yticks(size=40)
+    
+    plt.figure()
+    plt.title('Histogram of difference to average dark', size=40)
+    plt.semilogy(bin_diff_cent, hist_diff, lw=3)
+    plt.grid()
+    plt.xlabel('Difference to average dark', size=40)
+    plt.ylabel('Count', size=40)
+    plt.xticks(size=40);plt.yticks(size=40)
