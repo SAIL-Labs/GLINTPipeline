27094b28a7f45f10e480e5d12d1b5261e6ad1c17
mamartinod <ma.martinod@free.fr>
2019-10-22 16:34:05 +1100

    mrdft

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, 'GLINTPipeline/')
+sys.path.insert(0, os.path.abspath('GLINTPipeline/'))
 
 
 # -- Project information -----------------------------------------------------
