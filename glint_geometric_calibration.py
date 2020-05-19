1e607c0da56087cd39cb141804ffb53e51811bde
mamartinod <ma.martinod@free.fr>
2020-05-19 15:44:15 +1000

    Major changes
    - Remove useless function
    - add offset in the gaussian profile to fit
    - Display the outputs individually and overplot the fitted position
    respect to the spectral axis

--- glint_geometric_calibration.py
+++ glint_geometric_calibration.py
@@ -37,7 +37,7 @@ import glint_classes
 from scipy.optimize import curve_fit
 #import skimage.measure as sk
 
-def gaussian(x, A, x0, sig):
+def gaussian(x, A, x0, sig, offset):
     """
     Computes a Gaussian curve
     
@@ -55,28 +55,8 @@ def gaussian(x, A, x0, sig):
         
         Gaussian curve.
     """    
-    return A * np.exp(-(x-x0)**2/(2*sig**2))# + offset
+    return A * np.exp(-(x-x0)**2/(2*sig**2)) + offset
 
-def polynom(x, *args):
-    """
-    Computes a polynom from the coefficients in ``*args`` and returns the evaluation for ``x``.
-    
-    :Parameters:
-        
-        **x**: values where the curve is estimated.
-        
-        **a**: amplitude of the Gaussian.
-        
-        **x0**: location of the Gaussian.
-        
-        **sig**: scale of the Gaussian.
-        
-    :Returns:
-        
-        Gaussian curve.
-    """    
-    p = np.poly1d([*args])
-    return p(x)
 
 if __name__ == '__main__':
     # =============================================================================
@@ -87,10 +67,14 @@ if __name__ == '__main__':
     
     print("Getting the shape (position and width) of all tracks")
     ''' Inputs '''
-    datafolder = '20200201/AlfBoo/'
+    datafolder = '20191212/'
     root = "/mnt/96980F95980F72D3/glint/"
     data_path = '/mnt/96980F95980F72D3/glint_data/'+datafolder
-    data_list = [data_path+f for f in os.listdir(data_path) if 'AlfBoo' in f in f]
+    data_list = [data_path+f for f in os.listdir(data_path) if 'lab_turb' in f in f]
+    spectral_calibration_path = root+'GLINTprocessed/'+'calibration_params/'
+    wl_to_px_coeff = np.load(spectral_calibration_path+'wl_to_px.npy')
+    px_to_wl_coeff = np.load(spectral_calibration_path+'px_to_wl.npy')    
+    
     if len(data_list) == 0:
         raise IndexError('Data list is empty')
         
@@ -115,6 +99,7 @@ if __name__ == '__main__':
     sep = (y_ends[1] - y_ends[0])/(nb_tracks-1)
     channel_pos = np.around(np.arange(y_ends[0], y_ends[1]+sep, sep))
     
+    print('Averaging frames')
     for f in data_list[:]:
         img = glint_classes.Null(f)
         img.cosmeticsFrames(np.zeros(dark.shape))
@@ -126,14 +111,17 @@ if __name__ == '__main__':
     slices = slices / superNbImg
     super_img = super_img / superNbImg
    
-    plt.figure(0)
+    plt.figure(0, figsize=(19.2,10.8))
     plt.clf()
     plt.imshow(super_img-dark, interpolation='none', vmin=super_img.min(), vmax=5000)
     plt.colorbar()
+    plt.savefig(output_path+'geo_calib_avg_frame.png')
     
+    labels = ['P4', 'N3', 'P3', 'N2', 'AN4', 'N5', 'N4', 'N6', 'AN1', 'AN6', 'N1', 'AN2', 'P2', 'AN3', 'AN5', 'P1']
+
     ''' Fit a gaussian on every track and spectral channel
         to get their positions, widths and amplitude '''
-    
+    print('Determine position and width of the outputs')
     img = glint_classes.Null(data=None, nbimg=(0,1))
     img.cosmeticsFrames(np.zeros(dark.shape))
     img.getChannels(channel_pos, sep, spatial_axis)
@@ -144,107 +132,166 @@ if __name__ == '__main__':
     cov = []
     residuals = []
     
+    pattern = []
     for i in range(slices.shape[0]): # Loop over columns of pixel
         for j in range(slices.shape[1]): # Loop over tracks
-            p_init2 = np.array([100, channel_pos[j], 1.])
+            p_init2 = np.array([100, channel_pos[j], 1., 0])
             try:
                 popt, pcov = curve_fit(gaussian, slices_axes[j], slices[i,j], p0=p_init2)
                 params.append(popt)
                 cov.append(np.diag(pcov))
                 residuals.append(slices[i,j] - gaussian(slices_axes[j], *popt))
-                if i == 50 and j == 0:
-                    plt.figure();plt.plot(slices_axes[j], slices[i,j]);plt.plot(slices_axes[j], gaussian(slices_axes[j], *popt));plt.title('plop')
             except RuntimeError:
-                params.append(np.zeros(p_init2.shape))
-                cov.append(np.zeros(p_init2.shape))
+                popt = np.zeros(p_init2.shape)
+                pcov = np.zeros(p_init2.shape)
+                params.append(popt)
+                cov.append(pcov)
                 residuals.append(np.zeros(slices_axes[j].shape))
                 print("Error fit at spectral channel "+str(i)+" of track "+str(j))
+            reconstruction = gaussian(slices_axes[j], *popt)
+            pattern.append(reconstruction)
+            if i == 40 or i == 60 or i == 85:
+                plt.figure(1, figsize=(19.2,10.8))
+                plt.subplot(4,4,j+1)
+                plt.plot(slices_axes[j], slices[i,j], '.')
+                plt.plot(slices_axes[j], gaussian(slices_axes[j], *popt))
+                plt.grid()
+                plt.title(labels[j])
+    plt.tight_layout()        
+    plt.savefig(output_path+'geo_calib_fitting_output.png')
     
     params = np.array(params).reshape((dark.shape[1],nb_tracks,-1))
     cov = np.array(cov).reshape((dark.shape[1],nb_tracks,-1))
     residuals = np.array(residuals).reshape((dark.shape[1],nb_tracks,-1))
+    pattern = np.array(pattern).reshape((dark.shape[1],nb_tracks,-1))
     
     params[:,:,2] = abs(params[:,:,2]) # convert negative widths into positive ones
+    pattern = np.zeros_like(slices)
     
-    ''' Fit functions on positions and widths vector to extrapolate on any point 
-        along the spectral axis '''
+    plt.figure(2, figsize=(19.2,10.8))
+    plt.clf()
+    for k in range(16):
+        spectral_axis_in_nm = np.poly1d(px_to_wl_coeff[k])(spectral_axis)
+        step = np.mean(np.diff(spectral_axis_in_nm))
+        plt.subplot(4,4,k+1)
+        plt.imshow(np.log10(slices[:,k,:].T), interpolation='none', extent=[spectral_axis_in_nm[0]-step/2, spectral_axis_in_nm[-1]+step/2, img.slices_axes[k,-1]-0.5, img.slices_axes[k,0]+0.5], aspect='auto')
+        plt.colorbar()
+        plt.plot(spectral_axis_in_nm[20:], params[20:,k,1])
+        plt.title(labels[k])
+        plt.xlabel('Wavelength (nm)')
+        plt.ylabel('Vertical position')
+    plt.tight_layout()
+    ppp
+    plt.savefig(output_path+'geo_calib_avg_slices.png')
+    plt.figure(3, figsize=(19.2,10.8))
+    plt.clf()
+    for k in range(16):
+        spectral_axis_in_nm = np.poly1d(px_to_wl_coeff[k])(spectral_axis)
+        step = np.mean(np.diff(spectral_axis_in_nm))
+        plt.subplot(4,4,k+1)
+        plt.imshow(slices[:,k,:].T, interpolation='none', aspect='auto')
+        plt.colorbar()
+        plt.title(labels[k])
+        plt.xlabel('Wavelength (px)')
+        plt.ylabel('Vertical position (px)')
+    plt.tight_layout()
+    plt.savefig(output_path+'geo_calib_avg_slices_no_unit.png')
+    plt.figure(4, figsize=(19.2,10.8))
+    plt.clf()
+    for k in range(16):
+        spectral_axis_in_nm = np.poly1d(px_to_wl_coeff[k])(spectral_axis)
+        step = np.mean(np.diff(spectral_axis_in_nm))
+        plt.subplot(4,4,k+1)
+        plt.imshow(residuals[:,k].T, interpolation='none', aspect='auto')
+        plt.colorbar()
+        plt.title(labels[k])
+        plt.xlabel('Wavelength (nm)')
+        plt.ylabel('Vertical position')
+    plt.tight_layout()
+    plt.savefig(output_path+'geo_calib_residuals.png')
+
+    if save:
+        np.save(output_path+'pattern_coeff', params)
+    
+    # ''' Fit functions on positions and widths vector to extrapolate on any point 
+    #     along the spectral axis '''
         
-    bounds = [33, 96] # Cut the noisy part of the spectral
-    pos = params[bounds[0]:bounds[1],:,1]
-    wi = params[bounds[0]:bounds[1],:,2]
+    # bounds = [33, 96] # Cut the noisy part of the spectral
+    # pos = params[bounds[0]:bounds[1],:,1]
+    # wi = params[bounds[0]:bounds[1],:,2]
     
-    weight_pos = np.ones(pos.shape)
-    weight_width = np.ones(wi.shape)
+    # weight_pos = np.ones(pos.shape)
+    # weight_width = np.ones(wi.shape)
     
-    # weight_pos[80-bounds[0]:,1] = 1.e-36
-    # weight_pos[80-bounds[0]:,2] = 1.e-36
-    # weight_pos[85-bounds[0]:,7] = 1.e-36
-    # weight_pos[84-bounds[0]:,13] = 1.e-36
-    # weight_pos[78-bounds[0],15] = 1.e-36
+    # # weight_pos[80-bounds[0]:,1] = 1.e-36
+    # # weight_pos[80-bounds[0]:,2] = 1.e-36
+    # # weight_pos[85-bounds[0]:,7] = 1.e-36
+    # # weight_pos[84-bounds[0]:,13] = 1.e-36
+    # # weight_pos[78-bounds[0],15] = 1.e-36
     
-    # weight_width[36-bounds[0]:80-bounds[0],:] = 1000
-    # weight_width[36-bounds[0]:,3] = 1000
-    # weight_width[36-bounds[0]:,4] = 1000
-    # weight_width[36-bounds[0]:40-bounds[0],5] = 1
-    # weight_width[36-bounds[0]:,6] = 1000
-    # weight_width[36-bounds[0]:,7] = 1000
-    # weight_width[36-bounds[0]:,8] = 1000
-    # weight_width[36-bounds[0]:,9] = 1000
-    # weight_width[36-bounds[0]:,10] = 1000
-    # weight_width[36-bounds[0]:40-bounds[0],11] = 1
-    # weight_width[36-bounds[0]:40-bounds[0],12] = 1
-    # weight_width[80-bounds[0]:89-bounds[0],12] = 1000
-    # weight_width[77-bounds[0],12] = 1
-    # weight_width[36-bounds[0]:40-bounds[0],13] = 1
-    # weight_width[36-bounds[0]:40-bounds[0],14] = 1
-    # weight_width[36-bounds[0]:40-bounds[0],15] = 1
-    # weight_width[78-bounds[0],15] = 1
+    # # weight_width[36-bounds[0]:80-bounds[0],:] = 1000
+    # # weight_width[36-bounds[0]:,3] = 1000
+    # # weight_width[36-bounds[0]:,4] = 1000
+    # # weight_width[36-bounds[0]:40-bounds[0],5] = 1
+    # # weight_width[36-bounds[0]:,6] = 1000
+    # # weight_width[36-bounds[0]:,7] = 1000
+    # # weight_width[36-bounds[0]:,8] = 1000
+    # # weight_width[36-bounds[0]:,9] = 1000
+    # # weight_width[36-bounds[0]:,10] = 1000
+    # # weight_width[36-bounds[0]:40-bounds[0],11] = 1
+    # # weight_width[36-bounds[0]:40-bounds[0],12] = 1
+    # # weight_width[80-bounds[0]:89-bounds[0],12] = 1000
+    # # weight_width[77-bounds[0],12] = 1
+    # # weight_width[36-bounds[0]:40-bounds[0],13] = 1
+    # # weight_width[36-bounds[0]:40-bounds[0],14] = 1
+    # # weight_width[36-bounds[0]:40-bounds[0],15] = 1
+    # # weight_width[78-bounds[0],15] = 1
 
     
     
-    coeff_position_poly = np.array([np.polyfit(spectral_axis[bounds[0]:bounds[1]], pos[:,i], deg=4, w=weight_pos[:,i]) for i in range(nb_tracks)])
-    coeff_width_poly = np.array([np.polyfit(spectral_axis[bounds[0]:bounds[1]], wi[:,i], deg=4, w=weight_width[:,i]) for i in range(nb_tracks)])
-    position_poly = [np.poly1d(coeff_position_poly[i]) for i in range(nb_tracks)]
-    width_poly = [np.poly1d(coeff_width_poly[i]) for i in range(nb_tracks)]
+    # coeff_position_poly = np.array([np.polyfit(spectral_axis[bounds[0]:bounds[1]], pos[:,i], deg=4, w=weight_pos[:,i]) for i in range(nb_tracks)])
+    # coeff_width_poly = np.array([np.polyfit(spectral_axis[bounds[0]:bounds[1]], wi[:,i], deg=4, w=weight_width[:,i]) for i in range(nb_tracks)])
+    # position_poly = [np.poly1d(coeff_position_poly[i]) for i in range(nb_tracks)]
+    # width_poly = [np.poly1d(coeff_width_poly[i]) for i in range(nb_tracks)]
     
-    for i in range(nb_tracks)[:]:
-        fitted_pos = position_poly[i](spectral_axis[:])
-        plt.figure()
-        ax = plt.gca()
-        color = next(ax._get_lines.prop_cycler)['color']
-        marker, marker2 = '+-', '.'
-        plt.subplot(221)
-        plt.plot(spectral_axis[:], params[:,i,1], marker, lw=3, label='Position')
-        plt.plot(spectral_axis[:], fitted_pos, marker2, label='Fit')
-        plt.grid()
-        plt.legend(loc='best', ncol=4)
-        plt.ylim(fitted_pos.min()*0.999, fitted_pos.max()*1.001)
-        plt.title('x0 / Track '+str(i+1))
-        plt.ylabel('x0')
-        plt.xlabel('Wavelength')
-        plt.subplot(223)
-        plt.plot(spectral_axis[:], (params[:,i,1]-position_poly[i](spectral_axis[:]))/params[:,i,1]*100)
-        plt.grid()
-        plt.xlabel('Wavelength')
-        plt.ylabel('Residual (%)')
-        plt.ylim(-1, 1)
-        plt.subplot(222)
-        plt.plot(spectral_axis, params[:,i,2], marker, lw=3, label='Width')
-        plt.plot(spectral_axis, width_poly[i](spectral_axis), marker2, label='Fit')
-        plt.grid()
-        plt.legend(loc='best', ncol=4)
-        plt.title('sig / Track'+str(i+1))
-        plt.ylabel('Sig')
-        plt.xlabel('Wavelength')
-        plt.xlim(0)
-        plt.ylim(0,2)
-        plt.subplot(224)
-        plt.plot(spectral_axis, (params[:,i,2]-width_poly[i](spectral_axis))/params[:,i,2]*100)
-        plt.grid()
-        plt.xlabel('Wavelength')
-        plt.ylabel('Residual (%)')
-        plt.ylim(-10,10)
+    # for i in range(nb_tracks)[:]:
+    #     fitted_pos = position_poly[i](spectral_axis[:])
+    #     plt.figure()
+    #     ax = plt.gca()
+    #     color = next(ax._get_lines.prop_cycler)['color']
+    #     marker, marker2 = '+-', '.'
+    #     plt.subplot(221)
+    #     plt.plot(spectral_axis[:], params[:,i,1], marker, lw=3, label='Position')
+    #     plt.plot(spectral_axis[:], fitted_pos, marker2, label='Fit')
+    #     plt.grid()
+    #     plt.legend(loc='best', ncol=4)
+    #     plt.ylim(fitted_pos.min()*0.999, fitted_pos.max()*1.001)
+    #     plt.title('x0 / Track '+str(i+1))
+    #     plt.ylabel('x0')
+    #     plt.xlabel('Wavelength')
+    #     plt.subplot(223)
+    #     plt.plot(spectral_axis[:], (params[:,i,1]-position_poly[i](spectral_axis[:]))/params[:,i,1]*100)
+    #     plt.grid()
+    #     plt.xlabel('Wavelength')
+    #     plt.ylabel('Residual (%)')
+    #     plt.ylim(-1, 1)
+    #     plt.subplot(222)
+    #     plt.plot(spectral_axis, params[:,i,2], marker, lw=3, label='Width')
+    #     plt.plot(spectral_axis, width_poly[i](spectral_axis), marker2, label='Fit')
+    #     plt.grid()
+    #     plt.legend(loc='best', ncol=4)
+    #     plt.title('sig / Track'+str(i+1))
+    #     plt.ylabel('Sig')
+    #     plt.xlabel('Wavelength')
+    #     plt.xlim(0)
+    #     plt.ylim(0,2)
+    #     plt.subplot(224)
+    #     plt.plot(spectral_axis, (params[:,i,2]-width_poly[i](spectral_axis))/params[:,i,2]*100)
+    #     plt.grid()
+    #     plt.xlabel('Wavelength')
+    #     plt.ylabel('Residual (%)')
+    #     plt.ylim(-10,10)
     
-    if save:
-        np.save(output_path+'coeff_position_poly', coeff_position_poly)
-        np.save(output_path+'coeff_width_poly', coeff_width_poly)
+    # if save:
+    #     np.save(output_path+'coeff_position_poly', coeff_position_poly)
+    #     np.save(output_path+'coeff_width_poly', coeff_width_poly)
