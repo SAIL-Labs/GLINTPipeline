fa9221a4fe15d893442a994b3bc2ef3e874a306d
mamartinod <ma.martinod@free.fr>
2020-06-26 16:52:41 +1000

    Save plots on demand and change in the model to fit: Gaussian + offset

--- glint_spectral_calibration.py
+++ glint_spectral_calibration.py
@@ -27,7 +27,6 @@ First step: simply change the value of the variables in the section **Settings**
     
 Second step: change the value of the variables in the sections **Inputs**, **Outputs** and **Iterate on wavelength**:
     * **datafolder**: folder containing the datacube to use.
-    * **root**: path to **datafolder**.
     * **data_list**: list of files in **datafolder** to open.
     * **output_path**: path to the folder where the products are saved.
     * **calibration_path**: path to the calibration files used to process the file (location, width of the outputs, etc.).
@@ -43,7 +42,7 @@ import glint_classes
 from scipy.optimize import curve_fit
 #import skimage.measure as sk
 
-def gaussian(x, A, x0, sig):
+def gaussian(x, A, x0, sig, offset):
     """
     Computes a Gaussian curve
     
@@ -61,91 +60,96 @@ def gaussian(x, A, x0, sig):
         
         Gaussian curve.
     """    
-    return A * np.exp(-(x-x0)**2/(2*sig**2))# + offset
+    return A * np.exp(-(x-x0)**2/(2*sig**2)) + offset
 
 if __name__ == '__main__':
     ''' Settings '''
-    save = False
-    
+    save = True
     
     ''' Inputs '''
     print("-----------------------------\nSpectral calibration")
-    datafolder = '201806_wavecal/'
-    root = "/mnt/96980F95980F72D3/glint/"
+    datafolder = 'data202006/wavecal/'
     data_path = '/mnt/96980F95980F72D3/glint_data/'+datafolder
-    output_path = root+'reduction/'+datafolder
-    calibration_path = output_path
-    
-    coeff_position_poly = np.load(calibration_path+'coeff_position_poly.npy')
-    coeff_width_poly = np.load(calibration_path+'coeff_width_poly.npy')
-    
+    output_path = '/mnt/96980F95980F72D3/glint/GLINTprocessed/'+datafolder
+
     ''' Output '''
-    output_path = root+'reduction/'+datafolder
     if not os.path.exists(output_path):
         os.makedirs(output_path)
     
     ''' Iterate on wavelength '''
-    wavelength = [1350, 1450, 1550, 1650, 1700][1:]
-    data_list = [[data_path+f for f in os.listdir(data_path) if not 'dark' in f and str(wl) in f] for wl in wavelength]
+    wavelength = [1400, 1450, 1500, 1550, 1600]
+    data_list0 = [[data_path+f for f in os.listdir(data_path) if '1400' in f],
+                 [data_path+f for f in os.listdir(data_path) if '1450bis' in f],
+                 [data_path+f for f in os.listdir(data_path) if '1500bis' in f],
+                 [data_path+f for f in os.listdir(data_path) if '1550bis' in f],
+                 [data_path+f for f in os.listdir(data_path) if '1600' in f]]
+
+    wavelength = [1500, 1550]
+    data_list0 = [[data_path+f for f in os.listdir(data_path) if '1500bis' in f],
+                 [data_path+f for f in os.listdir(data_path) if '1550bis' in f]]
     
     ''' Remove dark from the frames and average them to increase SNR '''
     dark = np.load(output_path+'superdark.npy')
+    dark_per_channel = np.load(output_path+'superdarkchannel.npy')
+    super_img = np.zeros(dark.shape)
+    superNbImg = 0.
     
     ''' Run '''
-    nb_tracks = 16
+    spatial_axis = np.arange(dark.shape[0])
+    spectral_axis = np.arange(dark.shape[1])
+    slices = np.zeros_like(dark_per_channel)
+    
+    ''' Define bounds of each track '''
     y_ends = [33, 329] # row of top and bottom-most Track
+    nb_tracks = 16
     sep = (y_ends[1] - y_ends[0])/(nb_tracks-1)
     channel_pos = np.around(np.arange(y_ends[0], y_ends[1]+sep, sep))
     
-    position_poly = [np.poly1d(coeff_position_poly[i]) for i in range(nb_tracks)]
-    width_poly = [np.poly1d(coeff_width_poly[i]) for i in range(nb_tracks)]
-    
     calib_pos = []
-    
-    for elt in data_list[:]:
-        super_img = np.zeros(dark.shape)
-        superNbImg = 0.
-         
-        for f in elt:
-            img = glint_classes.File(f)        
-            img.data = img.data - dark
+    for data_list in data_list0:
+        print('Processing wavelength %s'%(wavelength[data_list0.index(data_list)]))
+        print('Averaging frames')
+        for f in data_list[:]:
+            img = glint_classes.Null(f)
+            img.cosmeticsFrames(np.zeros(dark.shape))
+            img.getChannels(channel_pos, sep, spatial_axis, dark=dark_per_channel)
             super_img = super_img + img.data.sum(axis=0)
+            slices = slices + np.sum(img.slices, axis=0)
             superNbImg = superNbImg + img.nbimg
-        
+                
+        slices = slices / superNbImg
         super_img = super_img / superNbImg
-        super_img -= super_img[:25].mean()
-        spatial_axis = np.arange(super_img.shape[0])
-        spectral_axis = np.arange(super_img.shape[1])
     
-        plt.figure()
-        plt.imshow(super_img, interpolation='none', aspect='auto', vmin=-100, vmax=100)
-        plt.colorbar()
-        
-        slices = np.array([super_img[np.int(np.around(pos-sep/2)):np.int(np.around(pos+sep/2)),:] for pos in channel_pos])
-        slices_axes = np.array([spatial_axis[np.int(np.around(pos-sep/2)):np.int(np.around(pos+sep/2))] for pos in channel_pos])
+        labels = ['P4', 'N3', 'P3', 'N2', 'AN4', 'N5', 'N4', 'AN5', 'N6', 'AN1', 'AN6', 'N1', 'AN2', 'P2', 'AN3', 'P1']
     
-    #    sz_boxes = 20
-    #    maxi = np.array([np.unravel_index(np.argmax(s), s.shape) for s in slices])
-    #    boxes = [slices[i,:,int(maxi[i,1]-sz_boxes/2):int(maxi[i,1]+sz_boxes/2)+1] for i in range(slices.shape[0])]
-    #    moment = np.array([sk.moments(b, order=1) for b in boxes])
-    #    photocenter_spatial = np.array([m[1, 0] / m[0, 0] for m in moment]) - sep/2. + channel_pos
-    #    photocenter_wl = np.array([m[0, 1] / m[0, 0] for m in moment]) - sz_boxes/2 + maxi[:,1]
-    #    calib_pos.append(np.array([photocenter_wl, photocenter_spatial]))
-    #    plt.scatter(photocenter_wl, photocenter_spatial, marker='+', color='r')
+        ''' Fit a gaussian on every track and spectral channel
+            to get their positions, widths and amplitude '''
+        print('Determine position and width of the outputs')
+        img = glint_classes.Null(data=None, nbimg=(0,1))
+        img.cosmeticsFrames(np.zeros(dark.shape))
+        img.getChannels(channel_pos, sep, spatial_axis)
         
-        ygrid = np.array([np.around(p(spectral_axis)) for p in position_poly], dtype=np.int)
-        xgrid = np.meshgrid(spectral_axis, spectral_axis)[0][:nb_tracks]
-        tracks = super_img[ygrid, xgrid]
-        wl_pos = [] # Position of the spectral lines on the detector for each channel
+        img.slices = slices
+        slices_axes = img.slices_axes
+        tracks = img.slices[:,:,10-4:10+5].mean(axis=2) # Average along spatial axis
+        tracks = np.transpose(tracks)
+        
+        wl_pos = []
         for i in range(nb_tracks):
-            popt, pcov = curve_fit(gaussian, spectral_axis, tracks[i], p0=[100., spectral_axis[np.argmax(tracks[i])], 1.])
-            wl_pos.append(popt[1:])
-            if i ==2:
-                plt.figure();plt.plot(spectral_axis, tracks[i]);plt.plot(spectral_axis, gaussian(spectral_axis, *popt));plt.grid()
+            popt, pcov = curve_fit(gaussian, spectral_axis, tracks[i], p0=[tracks[i].max(), spectral_axis[np.argmax(tracks[i])], 1., 0])
+            wl_pos.append(popt[1:-1])
+            plt.figure(10+data_list0.index(data_list), figsize=(19.2, 10.8))
+            plt.subplot(4,4,i+1)
+            plt.plot(spectral_axis, tracks[i], '.')
+            plt.plot(spectral_axis, gaussian(spectral_axis, *popt))
+            plt.grid('on')
+            plt.title(labels[i])
+            plt.suptitle(str(wavelength[data_list0.index(data_list)]))
+            plt.tight_layout()
+            if save: plt.savefig(output_path+'fitting_%s'%(wavelength[data_list0.index(data_list)]))
         calib_pos.append(wl_pos)
     
     calib_pos = np.array(calib_pos)
-    
     coeff_poly_wl_to_px = np.array([np.polyfit(wavelength, calib_pos[:,i,0], deg=1) for i in range(nb_tracks)]) # detector resolution is around 5 nm/px
     coeff_poly_px_to_wl = np.array([np.polyfit(calib_pos[:,i,0], wavelength, deg=1) for i in range(nb_tracks)])
     poly_wl = [np.poly1d(coeff_poly_wl_to_px[i]) for i in range(nb_tracks)]
@@ -165,33 +169,12 @@ if __name__ == '__main__':
     for wl in wavelength:
         print(str(wl)+' nm -> '+str(wl/fwhm.mean(axis=1)[wavelength.index(wl)]))
     
-    plt.figure()
+    plt.figure(figsize=(19.2, 10.8))
     for i in range(nb_tracks):
         plt.subplot(4,4,i+1)
         plt.plot(wavelength, calib_pos[:,i,0], 'o')
         plt.plot(wavelength, poly_wl[i](wavelength))
         plt.grid()
         plt.title('Track %s'%(i+1))
-    #plt.figure()
-    #plt.grid()
-    #ax = plt.gca()
-    #for i in range(nb_tracks):
-    #    marker1, marker2 = 'o', '-'
-    #    if i > 9 : marker1, marker2 = 'd', '--'
-    #    color = next(ax._get_lines.prop_cycler)['color']
-    #    plt.plot(spectral_axis, position_poly[i](spectral_axis), marker2, c=color)
-    #    plt.plot(calib_pos[:,0,i], calib_pos[:,1,i], marker1, c=color)
-    #    plt.xlim(-5,100)
-    
-       
-    
-    #for i in range(nb_tracks):
-    #    plt.figure(i) 
-    #    plt.grid()
-    #    ax = plt.gca()
-    #    marker1, marker2 = 'o', '-'
-    #    if i > 9 : marker1, marker2 = 'd', '--'
-    #    color = next(ax._get_lines.prop_cycler)['color']
-    #    plt.plot(calib_pos[:,i,0], wavelength, marker1, c=color)
-    #    plt.plot(calib_pos[:,i,0], poly_wl[i](calib_pos[:,i,0]), marker2)
-    #    plt.ylim(1340, 1750)
+    plt.tight_layout()
+    if save: plt.savefig(output_path+'wl2px_%s'%(wavelength[data_list0.index(data_list)]))
